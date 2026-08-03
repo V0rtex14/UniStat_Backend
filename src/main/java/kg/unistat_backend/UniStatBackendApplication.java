715333db00fcd0a96fb4cbc50c1a19cf886a3aec
V0rtex14 <v0rtex14arb@gmail.com>
2026-08-03 16:29:11 +0600

    First Version

---
+++ src/main/java/kg/unistat_backend/UniStatBackendApplication.java
@@ -0,0 +1,13 @@
+package kg.unistat_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniStatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniStatBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/unistat_backend/UniStatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.unistat_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniStatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
